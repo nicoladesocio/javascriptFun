0eb4010ef4b581195ff653d74c464e68cd20ba36
nicoladesocio <nikdeso@hotmail.it>
2015-06-09 21:40:13 +0200

    perfezionamento e revisione

--- js/script.js
+++ js/script.js
@@ -1,93 +1,46 @@
-/*var droni = new Array()
-var countdown = 10
-var canvasW = 0
-var canvasH = 0;
-
-function loadGame() {
-   //prompt('shit hapens')
-   var canvas = document.getElementById('canvas');
-   canvas.style.height='500px';
-   canvasH = 500
-   canvasW = parseInt(canvas.style.width)
-   canvas.style.paddingRight='0px'
-   canvas.style.paddingLeft='0px'
-   setTimeout(anima(),30)
-}
-
-function nuovoDrone() {
-      var drone = document.createElement('div')
-      //drone.src='icons/flatty-social-media/png/chat23.png'
-      drone.className = 'icon facebook-icon';
-      drone.style.width = '20px';
-      drone.style.height = '20px';
-      drone.style.position = 'relative';
-      drone.style.left = 0 + 'px'
-      drone.style.top = Math.floor(Math.random()*475) +'px'
-      var canvas = document.getElementById('canvas');
-      canvas.appendChild(drone);
-      drone.parentNode = canvas;
-      droni.push(drone)
-}
-
-function spostaDrone(droneID) {
-   var drone = droni[droneID]
-   var pos = parseInt(drone.style.left) + 1
-   if (pos > 1000) {
-      var canvas = document.getElementById('canvas');
-      drone.parentNode.removeChild(drone);
-      droni.splice(droneID,1)
-   }
-   drone.style.left = pos
-}
-
-function anima() {
-   if(countdown != 0) {
-      nuovoDrone()
-      countdown--;
-   }
-   for (var i = 0; i < droni.length; i++) {
-    var uscito = spostaDrone(i);
-  }
-   if(droni.length > 0) {
-      setTimeout(anima(),1000)
-   }
-}
-*/
-var scatoline = new Array();
-var Icone = new Array('facebook','twitter','gplus','skype','youtube','instagram')
+var Icone = new Array();
+var NomiIcone = new Array('facebook','twitter','gplus','skype','youtube','instagram')
 var countdown = 6
 var canvasW = 0
 var canvasH = 500
-var offset
-var margin = 20
-var id = 0
 var deg = 0
+var gravityX = 0
+var gravityY = 0
+var timer
 
-function creaScatolina() {
+function creaIcona() {
     var tela = document.getElementById('canvas');
   var telaQ = $('.canvas')
   var s = document.createElement('div');
-  s.className = 'icon ' + Icone[id] + '-icon';
-  s.style.top = 0 + 'px';
-  id++
+  s.className = 'icon ' + NomiIcone[countdown -1] + '-icon';
+  s.style.top = 50 + 'px';
   s.style.left = 0
-  s.Vx = (Math.random() * id) + 1;
-  s.Vy = 5;
-  //s.Ax = Math.random()/2 + 1;
-  //s.Ay = Math.random()/2 + 1
-  //s.animate(s.style.left,400)
+  s.Vx = 4;
+  s.Vy = 0;
   telaQ.append(s);
-  scatoline.push(s);
+  Icone.push(s);
 }
 
-function spostaScatolina(s) {
+function spostaIcona(s) {
   var xNew = parseInt(s.style.left) + s.Vx;
   var yNew = parseInt(s.style.top) + s.Vy;
-  if (xNew < 0 || xNew > canvasW - 50) s.Vx *= -1;
-  if (yNew > canvasH - 50) s.Vy *= -1;
+  if (xNew < 0){
+    s.Vx *= -1;
+    xNew = 0;
+  } else if (xNew > canvasW - 40) {
+    s.Vx *= -1
+    xNew = canvasW - 40;
+  }
+  if (yNew < 0){
+    s.Vy *= -1;
+    yNew = 0;
+  } else if (yNew > canvasH - 40) {
+    s.Vy *= -1;
+    yNew = canvasH - 40;
+  }
   //console.log("a " + x + " b " + y + " w " + canvasW)
-  s.style.left = (xNew + s.Vx) + 'px';
-  s.style.top = (yNew + s.Vy) + 'px';
+  s.style.left = xNew + 'px';
+  s.style.top = yNew + 'px';
   //s.Vx += s.Ax;
   deg++
   s.style.webkitTransform = 'rotate('+deg+'deg)'; 
@@ -95,26 +48,32 @@ function spostaScatolina(s) {
     s.style.msTransform     = 'rotate('+deg+'deg)'; 
     s.style.oTransform      = 'rotate('+deg+'deg)'; 
     s.style.transform       = 'rotate('+deg+'deg)';
-  s.Vy += 1;
+    s.Vy += gravityY
+    s.Vx += gravityX
 }
+
 function restart() {
   var canvas = document.getElementById('canvas');
-  for (var i = 0; i < scatoline.length; i++) {
-    canvas.removeChild(scatoline[i])
+  for (var i = 0; i < Icone.length; i++) {
+    canvas.removeChild(Icone[i])
   } 
-  scatoline = new Array()
-  countdown = 10
+  Icone = new Array()
+  countdown = 6
+  gravityX = 0
+  gravityY = 1
+  clearTimeout(timer)
+  loadGame()
 }
 
 function anima() {
    if(countdown > 0) {
-      creaScatolina()
+      creaIcona()
       countdown--;
    }
-  for (var i = 0; i < scatoline.length; i++) {
-    spostaScatolina(scatoline[i]);
+  for (var i = 0; i < Icone.length; i++) {
+    spostaIcona(Icone[i]);
   }
-  setTimeout('anima()', 30);
+  timer = setTimeout('anima()', 30);
 }
 
 function loadGame() {
@@ -122,15 +81,55 @@ function loadGame() {
   var sprite = document.createElement('div')
   sprite.className = 'square-small'
   sprite.style.backgroundColor = 'red'
-   canvas.style.height='500px'
-   setTimeout('anima()', 30);
+   canvas.style.height= '500px';
+   gravityX = 0
+   gravityY = 1
+   timer = setTimeout('anima()', 30);
    var canvas = $('.canvas')
    canvasW = canvas.width()
    canvasH = canvas.height()
    
-   sprite.style.left = canvasW/2 + 'px'
-   sprite.style.top= (canvasH-20) + 'px'
-   
-   canvas.append(sprite)
-   
+   document.body.focus()
+}
+ 
+document.onkeydown = function(e) {
+  e = (e ? e : window.event);
+  var key = (e.keyCode ? e.keyCode : (e.which ? e.which : false))
+  console.log(key)
+  flow = true
+  if (key == '37') {
+    //LEFT
+    gravityX = -1;
+    gravityY = 0;
+  }
+  else if (key == '38') {
+    //UP
+    gravityX = 0;
+    gravityY = -1
+  }
+  else if(key == '39') {
+    //RIGHT
+    gravityX = 1;
+    gravityY = 0;
+  }
+  else if (key == '40') {
+    //DOWN
+    gravityX = 0
+    gravityY = 1
+  }
+  resetGravity(gravityX,gravityY)
+}
+function resetGravity(valueX,valueY) {
+  for (var i = 0; i < Icone.length; i++) {
+    if(valueX != 0) {
+      Icone[i].Vx = 0;
+    }
+    else {
+      Icone[i].Vy = 0;
+    }
+  }
+}
+
+document.onkeyup = function(e) {
+  speedIncr = 1
 }
